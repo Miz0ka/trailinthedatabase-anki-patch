28e4317e56464905a6b5ea2dd68dcd1977a19ece
Ossan <23367859+Miz0ka@users.noreply.github.com>
2023-11-05 17:03:24 +0100

    add the script and Readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # trailinthedatabase-anki-patch
-Patch anki card to add sound from trailinthedatabase
+
+Patch your last anki note to add the selected sound from trailinthedatabase.com
+
+![Image](./README/update.png)
+
+## How to Install
+
+### AnkiConnect
+
+Install the anki-connect plugin (https://ankiweb.net/shared/info/2055492159).
+
+### Script
+
+#### 1.
+Download the **tampermonkey** extension or an equivalent:
+- Chrome: https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en
+- Firefox: https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/
+
+#### 2.
+<details>
+  <summary>Click on the extension, then <b>Create New Script</b></summary>
+
+  ![Image](./README/1.png)
+</details>
+
+#### 3.
+Copy & paste the content of the [trailinthedatabase-anki-patch.user.js](./trailinthedatabase-anki-patch.user.js) file, and save it
+
+#### 4.
+Update parameters to your environment
+- Ankiconnect address
+- Name of audio the field on your note
+
+#### 5.
+<details>
+  <summary>Now you can go on <a href="https://trailinthedatabase.com/">https://trailinthedatabase.com/</a> and toggle the script by clicking on the icon</summary>
+
+  ![Image](./README/2.png)
+</details>
---
+++ trailinthedatabase-anki-patch.user.js
@@ -0,0 +1,194 @@
+// ==UserScript==
+// @name         trailinthedatabase-anki
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Patch anki card to add sound from trailinthedatabase
+// @author       Ossan
+// @match        *://trailsinthedatabase.com/*
+// @icon         https://trailsinthedatabase.com/favicon.ico
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+/*
+  This userscript requires anki-connect plugin to allow the user to interact with his flashcard.
+
+  References:
+  - https://foosoft.net/projects/anki-connect
+*/
+
+// ------------  PARAMS ----------
+var ankiNoteAudioFieldName = "SentAudio";
+var ankiConnectURL = "http://127.0.0.1:8765";
+// -------------------------------
+
+(function() {
+    console.log("TamperMonkey script: trailinthedatabase-anki installed");
+    setTimeout(updateTable, 1500);
+})();
+
+
+function updateTable(){
+    var table = document.getElementsByClassName("table")[0];
+    var audioList = table.getElementsByTagName("audio");
+    var styles = `
+        color: rgb(21, 136, 62);
+        background: none;
+        font-weight: 700;
+        border: 0px;
+        border-radius: 0px;`
+    for (let audio of audioList) {
+        var button = document.createElement("button");
+        button.innerHTML = "+";
+        button.style= styles;
+        button.onclick = function() { updateLastAnkiNote(audio.currentSrc); };
+        audio.insertAdjacentElement("afterend", button)
+    }
+    console.log("TamperMonkey script: update table");
+}
+
+function updateLastAnkiNote(audioURL){
+    let lastCreatedNotesRequest = {
+        "action": "findNotes",
+        "version": 6,
+        "params": {
+            "query": "added:1"
+        }
+    };
+
+    GM_xmlhttpRequest({
+      method: "POST",
+      url: ankiConnectURL,
+      data: JSON.stringify(lastCreatedNotesRequest),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      onload: function(response) {
+            var noteList = JSON.parse(response.responseText);
+            if(noteList.result.length == 0){
+                alert("No new note detected")
+                return;
+            }
+            let noteId=noteList.result.pop();
+
+            guiBrowserUnFocus();
+            updateNoteTag(noteId);
+            updateNoteField(noteId, audioURL);
+            guiBrowserFocusUpdatedNote(noteId);
+        },
+      onerror: function(response){
+         alert("A problem occured. Possible causes : Anki is not running or the extension anki-connect or your port 8765 is not available. Check in your webbrowser that this address do not 404 : http://127.0.0.1:8765/");
+      },
+    });
+}
+
+function updateNoteTag(noteId){
+    let tagName="Sore-no-kiseki"
+    let addNoteTagRequest = {
+        "action": "addTags",
+        "version": 6,
+        "params": {
+            "notes": [noteId],
+            "tags": tagName
+        }
+    }
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: ankiConnectURL,
+        data: JSON.stringify(addNoteTagRequest),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+            console.log(`${noteId} have been tagged [${tagName}]`);
+          },
+        onerror: function(response){
+           alert("A problem occured with the tag request.");
+        },
+      });
+}
+
+function updateNoteField(noteId, audioURL){
+    let fileName = audioURL.slice(audioURL.indexOf('talk/')).replace(/[^A-Za-z0-9]/g, "_")
+    let updateNote = {
+        "action": "updateNoteFields",
+        "version": 6,
+        "params": {
+            "note": {
+                "id": noteId,
+                "fields": {
+                },
+            	"audio": [{
+                    "url": audioURL,
+                    "filename": fileName,
+                    "fields": [
+                        ankiNoteAudioFieldName
+                    ]
+                }]
+            }
+        }
+    }
+
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: ankiConnectURL,
+        data: JSON.stringify(updateNote),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+            console.log(`${noteId} have been update with '${audioURL}'`);
+          },
+        onerror: function(response){
+           alert("A problem occured with the tag request.");
+        },
+      });
+}
+
+function guiBrowserUnFocus(){
+    let guiBrowserControl = {
+        "action": "guiBrowse",
+        "version": 5,
+        "params": {
+            "query": "nid:1"
+        }
+    }
+
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: ankiConnectURL,
+        data: JSON.stringify(guiBrowserControl),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+          },
+        onerror: function(response){
+           alert("A problem occured with the unfocus request.");
+        },
+      });
+}
+
+
+function guiBrowserFocusUpdatedNote(noteId){
+    let guiBrowserControl = {
+        "action": "guiBrowse",
+        "version": 5,
+        "params": {
+            "query": "nid:"+noteId
+        }
+    }
+
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: ankiConnectURL,
+        data: JSON.stringify(guiBrowserControl),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        onload: function(response) {
+          },
+        onerror: function(response){
+           alert("A problem occured with the Gui Browser.");
+        },
+      });
+}
